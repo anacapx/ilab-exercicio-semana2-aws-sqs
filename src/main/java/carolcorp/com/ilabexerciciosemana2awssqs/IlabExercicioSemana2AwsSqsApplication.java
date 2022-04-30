02e30ab234c12750f598248fb15ca1e6d4990150
amaral.carolina <w.anapereira@gmail.com>
2022-04-30 19:17:49 -0300

    First commit

---
+++ src/main/java/carolcorp/com/ilabexerciciosemana2awssqs/IlabExercicioSemana2AwsSqsApplication.java
@@ -0,0 +1,13 @@
+package carolcorp.com.ilabexerciciosemana2awssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlabExercicioSemana2AwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlabExercicioSemana2AwsSqsApplication.class, args);
+	}
+
+}
